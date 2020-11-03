82ee8b28893d57a520d9ecb7997f3dd7d00b31bf
lucas luz feitosa <lucasluz@fortalsistemas.com.br>
2020-11-03 09:36:37 -0300

    tutorial part 3 templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,22 @@ from django.urls import path, re_path
 
 from . import views
 
+# urlpatterns = [
+#     # ex: /polls/test/
+#     path('test/', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('specifics/<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+
+app_name = 'polls'
 urlpatterns = [
-    path('test', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
